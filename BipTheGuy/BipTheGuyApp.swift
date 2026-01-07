c32138a99c0a3a653cb1ec0fd8f6c45b41045a7b
Jim Walejko <jwalejko@me.com>
2026-01-06 21:20:12 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Jim Walejko on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
