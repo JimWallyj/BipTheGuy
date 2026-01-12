3286623c75291d8034fa04be97a90786b9ff8e32
Jim Walejko <jwalejko@me.com>
2026-01-11 20:39:19 -0700

    Prof. Gallaugher's SwiftUI programming course - Chapter 3.3 - Bip App = Native SwiftUI PhotosPicker, PhotoPickerItem type, .loadTransferrable to convert from a PhotoPickerItem to an Image View, Converted a PhotosPickerItem to a SwiftUI Image View, async (with await and Task), try, try? and try!

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,15 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
    // @State private var scale = 1.0 // 100% scale, or original size
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
+    
     var body: some View {
         VStack {
             Spacer()
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -31,12 +35,20 @@ struct ContentView: View {
             
             
                Spacer()
-            Button {
-                //TODO: Button action here
-            } label: {
+            
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
-            
+            .onChange(of: selectedPhoto) {
+                Task{
+                    guard let selectedImage = try? await
+                            selectedPhoto?.loadTransferable(type: Image.self) else{
+                        print("😡 ERROR: Could not get Image from loadTransferable")
+                        return
+                    }
+                    bipImage = selectedImage
+                }
+            }
         }
         .padding()
     }
